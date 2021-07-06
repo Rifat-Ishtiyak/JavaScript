44967ccf3f2d433c25a0c81c21111dafbc607f7e
Rifat-Ishtiyak <rifatishtiyak@gmail.com>
2021-07-06 13:16:45 +0600

    string template

---
+++ ES6/template.js
@@ -0,0 +1,11 @@
+
+const firstName="Justin";
+const lastName="TimberLake";
+const fullName=`${firstName} ${lastName} is a good boy`;
+console.log(fullName);
+
+
+const multiLine=`Yo bro     
+asc
+This is multiline.`
+console.log(multiLine);
