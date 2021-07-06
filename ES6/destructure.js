43684f8049ef4cef3333000186fea5c56aab397d
Rifat-Ishtiyak <rifatishtiyak@gmail.com>
2021-07-06 21:58:56 +0600

    destructure

---
+++ ES6/destructure.js
@@ -0,0 +1,14 @@
+
+const person={
+    name: 'jack William',
+    phone: '0123243566',
+    address: 'Malabar Hills',
+    friends: ['Tom Cruse', 'ABC', 'DEF'],
+    gf: 'Emma Watson'
+}
+
+let {address}=person;
+const {gf,phone}=person;
+
+console.log(address);
+console.log(gf,phone);
